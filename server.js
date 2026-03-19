88b1b7195becf69c1ba93ca465f6843949be91e8
Timothy Corbett-Clark <timothy@corbettclark.com>
2026-03-19 12:59:36 +0000

    Tidied bun dev server

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 import { join } from 'path'
 
+const port = 5173
 const dist = join(import.meta.dir, 'dist')
-const html = Bun.file(join(import.meta.dir, 'index.html'))
+const index_html = Bun.file(join(import.meta.dir, 'index.html'))
 
 Bun.serve({
-  port: 5173,
-  async fetch(req) {
-    const url = new URL(req.url)
-    const file = Bun.file(join(dist, url.pathname))
+  port: port,
+  fetch: async (req) => {
+    const url = new URL(req.url);
+    const file = Bun.file(join(dist, url.pathname));
     if (await file.exists()) {
       return new Response(file)
     }
-    return new Response(html, { headers: { 'Content-Type': 'text/html' } })
-  },
+    return new Response(index_html, { headers: { 'Content-Type': 'text/html' } })
+  }
 })
 
-console.log('Dev server running at http://localhost:5173')
+console.log(`Dev server running at http://localhost:${port}`)
