5510ea66da449252c7ad91c5cff7794ea58d122a
Timothy Corbett-Clark <timothy@corbettclark.com>
2026-04-01 15:00:32 +0100

    fix: synced the interface descriptions with appwrite

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,51 +5,63 @@ declare module '*.css' {
 
 export interface Trip {
   $id: string
-  code: string
-  description: string
-  location: string
-  startDate: string
-  endDate: string
   $createdAt: string
   $updatedAt: string
+  code: string
+  description: string
 }
 
 export interface Participant {
   $id: string
+  $createdAt: string
+  $updatedAt: string
   participantUserId: string
   participantUserName: string
   tripId: string
   role: 'coordinator' | 'participant'
-  $createdAt: string
 }
 
 export interface Proposal {
   $id: string
-  tripId: string
+  $createdAt: string
+  $updatedAt: string
   proposerUserId: string
   proposerUserName: string
+  tripId: string
   state: 'DRAFT' | 'SUBMITTED' | 'REJECTED' | 'APPROVED'
   title: string
   description: string
-  $createdAt: string
+  resortName: string
+  startDate: string
+  endDate: string
+  nearestAirport: string
+  transferTime: string
+  accommodationName: string
+  accommodationUrl: string
+  altitudeRange: string
+  country: string
+  approximateCost: string
 }
 
 export interface Poll {
   $id: string
-  tripId: string
+  $createdAt: string
+  $updatedAt: string
   pollCreatorUserId: string
   pollCreatorUserName: string
   state: 'OPEN' | 'CLOSED'
+  tripId: string
   proposalIds: string[]
-  $createdAt: string
 }
 
 export interface Vote {
   $id: string
+  $createdAt: string
+  $updatedAt: string
   pollId: string
   tripId: string
   voterUserId: string
+  voterUserName: string
   proposalIds: string[]
   tokenCounts: number[]
-  $createdAt: string
 }
